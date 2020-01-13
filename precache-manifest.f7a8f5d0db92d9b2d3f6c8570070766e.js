0362a7c950c5dfb5872d0757b9415fd4ff9375d0
Jeramey Normand <jnormand@2957.jnormand.user.nym2>
2020-01-13 21:26:58 +0000

    Updates

---
+++ precache-manifest.f7a8f5d0db92d9b2d3f6c8570070766e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694fd21414727e471f87edc7bb1562bc",
+    "url": "/react_prebid/index.html"
+  },
+  {
+    "revision": "876887fae35abaaf3236",
+    "url": "/react_prebid/static/css/main.67e8128e.chunk.css"
+  },
+  {
+    "revision": "935a67998e416e4ef88b",
+    "url": "/react_prebid/static/js/2.1e979f52.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_prebid/static/js/2.1e979f52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "876887fae35abaaf3236",
+    "url": "/react_prebid/static/js/main.cc808f71.chunk.js"
+  },
+  {
+    "revision": "07c37b96d785f27b649f",
+    "url": "/react_prebid/static/js/runtime-main.2740826f.js"
+  }
+]);
